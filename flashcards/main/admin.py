24d31629b5c3ac6e53ab30edcab395d51338cecc
Maciej Barelkowski <maciejbarel@gmail.com>
2017-07-13 15:15:58 +0200

    Register models in admin

--- flashcards/main/admin.py
+++ flashcards/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import Flashcard, Deck
+
+@admin.register(Flashcard)
+class FlashcardAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    pass
