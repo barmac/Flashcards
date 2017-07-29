d664a41f62a647808277d1ab7c5b49d59565d7da
Maciej Barelkowski <maciejbarel@gmail.com>
2017-07-29 21:18:46 +0200

    Add umlauts

--- flashcards/main/static/main/js/flashcards.js
+++ flashcards/main/static/main/js/flashcards.js
@@ -10,8 +10,14 @@ $(document).ready(function () {
         var question = button.parent().parent().find('.question').text();
         var answer = button.parent().parent().find('.answer').text();
         form.attr('action', '/flashcard/edit/' + id + '/');
-        form.find('[name="question"]').prop('value', question)
-        form.find('[name="answer"]').prop('value', answer)
+        form.find('[name="question"]').val(question)
+        form.find('[name="answer"]').val(answer)
         $('#submit').text('Edytuj fiszkę');
+    });
+    $('.special-letter-btn').on('click', function (event) {
+       event.preventDefault();
+       var btn = $(this);
+       var input = btn.closest('.input').find('input');
+       input.val(input.val() + btn.text());
     });
 });
