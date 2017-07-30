8c7656df7f3c2f721945ca4aa794ff9f503ca521
Maciej Barelkowski <maciejbarel@gmail.com>
2017-07-30 09:10:22 +0200

    Improve umlaut buttons script'

--- flashcards/main/static/main/js/flashcards.js
+++ flashcards/main/static/main/js/flashcards.js
@@ -10,14 +10,17 @@ $(document).ready(function () {
         var question = button.parent().parent().find('.question').text();
         var answer = button.parent().parent().find('.answer').text();
         form.attr('action', '/flashcard/edit/' + id + '/');
-        form.find('[name="question"]').val(question)
-        form.find('[name="answer"]').val(answer)
+        form.find('[name="question"]').val(question);
+        form.find('[name="answer"]').val(answer);
         $('#submit').text('Edytuj fiszkę');
     });
     $('.special-letter-btn').on('click', function (event) {
        event.preventDefault();
        var btn = $(this);
-       var input = btn.closest('.input').find('input');
-       input.val(input.val() + btn.text());
+       var input = btn.closest('.input').find('input').eq(0);
+       var caretPosStart = input.prop('selectionStart');
+       var caretPosEnd = input.prop('selectionEnd');
+       var inputValue = input.val();
+       input.val(inputValue.substring(0, caretPosStart) + btn.text() + inputValue.substring(caretPosEnd));
     });
 });
